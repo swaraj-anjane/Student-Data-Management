425421423aae0a8175e2657df0d170b6cd03b861
Swaraj <swarajanjane0@gmail.com>
2026-05-27 15:37:01 +0530

    mongdb connection update

--- Backend/config/connectdb.js
+++ Backend/config/connectdb.js
@@ -1,17 +1,28 @@
+// const mongoose = require("mongoose");
 
+// async function connectDatabase() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       serverSelectionTimeoutMS: 30000,
+//     });
 
+//     console.log("database connected successfully");
+//   } catch (error) {
+//     console.log("MongoDB Error:", error);
+//   }
+// }
+
+// module.exports = connectDatabase;
 
 const mongoose = require("mongoose");
 
 async function connectDatabase() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 30000,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("database connected successfully");
+    console.log(`Mongo Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("MongoDB Error:", error);
+    console.log("Mongo Error =>", error.message);
   }
 }
 
